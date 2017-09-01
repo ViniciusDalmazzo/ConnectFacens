0e3afb3ac6c4c2cd03c9184174593b44df52648b
Vine <vine>
2017-09-01 17:50:06 -0300

    Form Login/Registrar Editado
    -Página inicial editada;

--- index.php
+++ index.php
@@ -2,25 +2,232 @@
 $erro = isset($_GET['erro']) ? $_GET['erro'] : 0;
 ?>
 
-<!DOCTYPE HTML>
-<html lang="pt-br">
+<!doctype html>
+<html>
 <head>
-	<meta charset="UTF-8">
 
 	<title>Connect Facens</title>
-	<link rel="shortcut icon" href="imagens/icone.ico" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="lib/bootstrap2/css/bootstrap.min.css">
+	
+	<!-- jQuery library -->
+	<script src="lib/bootstrap2/js/jquery.min.js"></script>
+	
+	<!-- Bootstrap JavaScript -->
+	<script src="lib/bootstrap2/js/bootstrap.min.js"></script>
+	
+	<!-- Page CSS Files -->
+	<link rel="stylesheet" href="lib/tabs.css">
+	
+	<!--Font-Awesome CSS File--> 
+	<link rel="stylesheet" href="lib/bootstrap2/fa/css/font-awesome.min.css">
 
-	<!-- jquery - link cdn -->
-	<script src="lib/jquery/jquery.min.js"></script>
-	
-	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css">
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-	<link href="lib/fa/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
 
+<!--Container Starts-->
+<div class="container text-center">
+
+	<!--Row Starts-->
+	<div class="row">
 	
+		<!--Column Starts-->
+		<div class="col-md-6 col-md-offset-3">
+			
+			<!--Panel Starts-->
+			<div class="panel panel-default">
+				
+				<!--Panel Heading Starts-->
+				<div class="panel-heading nopadding">
+					<div class="row">
+						<div class="btn-group btn-group-justified">
+							<a href="#" class="btn btn-default btn-lg active" id="login_link">Entrar</a>
+							<a href="#" class="btn btn-default btn-lg" id="registration_link">Registrar</a>
+						</div>
+					</div>
+				</div>
+				<!--Panel Heading Ends-->
+				
+				<!--Panel Body Starts-->
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-lg-12 <?= $erro == 1 ? 'in' : '' ?>" aria=expanded="<?= $erro == 1 ? 'true' : 'false' ?>">
+						
+							<!--Login Form Starts-->
+							<form method="post" action="valida_usuario.php" id="login_form">
+								<div class="title"><h2>Connect Facens</h2></div>
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-envelope login_form_icons"></i></span>	
+											<input name="usuario" type="text" class="form-control input input-lg" placeholder="Seu usuário"  required>
+										</div>
+									</div>
+									
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-lock login_form_icons"></i></span>	
+											<input name="senha" type="password" class="form-control input input-lg" placeholder="Sua senha"  required>
+										</div>
+									</div>
+									
+									<div class="checkbox text-left">
+										<label><input type="checkbox"><span class="remember_me">Remember me</span></label>
+									</div>
+									
+									<div class="butn">
+										<button type="buttom" id="btn_login" class="btn btn-default btn-block btn-lg"><span class="glyphicon glyphicon-share-alt"></span> Login</button>
+									</div>
+									
+									<div class="erro">
+												<?php
+
+												if($erro == 1){
+													echo '<font color="red">Usuário e/ou senha inválido(s)</font>';
+												}											
+
+												?>
+											</div>
+							</form>
+							<!--Login Form Ends-->
+							
+							<!--Registration Form Starts-->
+							<form action=" registra_usuario.php" method="post"  id="registration_form">
+								<div class="title"><h2>Novo aqui? Registre-se</h2></div>
+									<div class="row">
+										<div class="col-xs-12 col-sm-6 col-md-6">
+											<div class="form-group">
+												<div class="input-group">
+													<span class="input-group-addon"><i class="glyphicon glyphicon-user registration_form_icons"></i></span>	
+													<input name="primeiroNome" type="text" class="form-control input input-lg" placeholder="Primeiro Nome"  required/>
+												</div>
+											</div>
+										</div>
+									
+										<div class="col-xs-12 col-sm-6 col-md-6">
+											<div class="form-group">
+												<div class="input-group">
+													<span class="input-group-addon"><i class="glyphicon glyphicon-user registration_form_icons"></i></span>	
+													<input name="ultimoNome" type="text" class="form-control input input-lg" placeholder="Ultimo Nome"  required/>
+												</div>
+											</div>
+										</div>
+									</div>
+									
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-envelope registration_form_icons"></i></span>	
+											<input name="email" type="email" class="form-control input input-lg" placeholder="Seu Email"  required>
+										</div>
+									</div>
+									
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-user registration_form_icons"></i></span>	
+											<input name="usuario" type="text" class="form-control input input-lg" placeholder="Seu Usuário"  required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-lock registration_form_icons"></i></span>	
+											<input name="senha" type="password" class="form-control input input-lg" placeholder="Sua Senha"  required>
+										</div>
+									</div>
+									
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-lock registration_form_icons"></i></span>	
+											<input type="password" class="form-control input input-lg" placeholder="Confirme a Senha"  required>
+										</div>
+									</div>
+									
+									<div class="checkbox text-left">
+										<label><input type="checkbox"><span class="check">I agree the terms & conditions.</span></label>
+									</div>
+									
+									<div class="butn">
+										<button type="submit" class="btn btn-default btn-block btn-lg"><span class="glyphicon glyphicon-share-alt"></span> Resgister</button>
+									</div>
+									
+									<div class="already_member">
+										<p>Already a User ? <a href="#">Log In Here</a></p>
+									</div>
+							</form>
+							<!--Registration Form Ends-->
+							
+						</div>
+					</div>
+				</div>
+				<!--Panel Body Ends-->
+				
+			</div>
+			<!--Panel Ends-->
+			
+			<!--Social Button Starts-->
+			<div class="social-buttons">
+				<div class="row">
+					<h4><b>Nos siga em outras redes sociais</b></h4>
+				</div>
+					
+				<div class="row">
+					<div class="col-sm-4">
+						<button class="btn btn-default btn-lg btn_facebook">
+							<i class="fa fa-facebook"></i> Facebook
+						</button>
+					</div>
 	
-	<script>
-		$(document).ready( function(){
+					<div class="col-sm-4">
+						<button class="btn btn-default btn-lg btn_twitter">
+							<i class="fa fa-twitter"></i> Twitter
+						</button>
+					</div>
+
+					<div class="col-sm-4">
+						<button class="btn btn-default btn-lg btn_gmail">
+							<i class="fa fa-google"></i> Gmail
+						</button>
+					</div>
+				</div>
+			</div>
+			<!--Social Button Ends-->
+			
+		</div>
+		<!--Column Ends-->
+		
+	</div>	
+	<!--Row Ends-->
+	
+	<div id="footer">
+		<p>Copyright © 2017 All Rights are Reserved. Designed By <a href="http://bootstraplayouts.com/">Connect Facens</a></p>
+	</div>
+	
+</div>
+<!--Container Ends-->
+
+<!--Script Starts-->
+<script>
+$(function() {
+
+    $('#login_link').click(function(e) {
+		$("#login_form").delay(100).fadeIn(100);
+ 		$("#registration_form").fadeOut(100);
+		$('#registration_link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#registration_link').click(function(e) {
+		$("#registration_form").delay(100).fadeIn(100);
+ 		$("#login_form").fadeOut(100);
+		$('#login_link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
+
+$(document).ready( function(){
 			$('#btn_login').click(function(){
 
 				var campo_vazio = false;
@@ -42,190 +249,9 @@ $erro = isset($_GET['erro']) ? $_GET['erro'] : 0;
 
 			});
 		});
-	</script>
-</head>
 
-<body>
-
-	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="">Connect Facens</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse <?= $erro == 1 ? 'in' : '' ?>" aria=expanded="<?= $erro == 1 ? 'true' : 'false' ?>">
-				
-				<ul class="nav navbar-nav navbar-right">
-					
-					<li class="dropdown <?= $erro == 1 ? 'open' : '' ?>">
-						<a href="" class="dropdown-toggle" data-toggle="dropdown">Entrar<span class="caret"></span></a>
-						<ul class="dropdown-menu dropdown-lr animated slideInRight" role="menu">
-							<div class="col-lg-12">
-								<div class="text-center"><h3><b>Entrar</b></h3></div>
-								<form method="post" action="valida_usuario.php" id="formLogin">
-									<div class="form-group">
-										<label for="username">Usuário</label>
-										<input type="text" name="usuario" id="campo_usuario" tabindex="1" class="form-control" placeholder="Username" value="" autocomplete="off">
-									</div>
-
-									<div class="form-group">
-										<label for="password">Senha</label>
-										<input type="password" name="senha" id="campo_senha" tabindex="2" class="form-control" placeholder="Password" autocomplete="off">
-									</div>
-
-									<div class="form-group">
-										<div class="row">
-											<div class="col-xs-7">
-												<input type="checkbox" tabindex="3" name="remember" id="remember">
-												<label for="remember"> Remember Me</label>
-											</div>
-											<div class="col-xs-5 pull-right">
-												<button type="buttom" class="btn btn-primary" id="btn_login">Entrar</button>
-											</div>
-											<br /><br />
-											<div class="erro">
-												<?php
-
-												if($erro == 1){
-													echo '<font color="red">Usuário e/ou senha inválido(s)</font>';
-												}											
-
-												?>
-											</div>
-										</div>
-									</div>
-									
-								</form>
-							</div>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-
-	<link rel="stylesheet" href="lib/style.css">
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600' rel='stylesheet' type='text/css'>
-	<link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet">
-
-	<div class="container">
-
-		<form class="well form-horizontal" action=" " method="post"  id="contact_form">
-			<fieldset>
-
-				<!-- Form Name -->
-				<legend>Novo aqui? Registre-se</legend>
-
-				<!-- Text input-->
-
-				<div class="form-group">
-					<label class="col-md-4 control-label">Usuario</label>  
-					<div class="col-md-4 inputGroupContainer">
-						<div class="input-group">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-							<input  name="first_name" placeholder="Usuario" class="form-control"  type="text">
-						</div>
-					</div>
-				</div>
-
-				<!-- Text input-->
-
-				<div class="form-group">
-					<label class="col-md-4 control-label" >Senha</label> 
-					<div class="col-md-4 inputGroupContainer">
-						<div class="input-group">
-							<span class="input-group-addon"><i class="fa fa-unlock-alt"></i></span>
-							<input name="last_name" placeholder="Senha" class="form-control"  type="password">
-						</div>
-					</div>
-				</div>
-
-				<!-- Text input-->
-				<div class="form-group">
-					<label class="col-md-4 control-label">E-Mail</label>  
-					<div class="col-md-4 inputGroupContainer">
-						<div class="input-group">
-							<span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-							<input name="email" placeholder="E-Mail" class="form-control"  type="text">
-						</div>
-					</div>
-				</div>
-				
-
-				<!-- radio checks -->
-				<div class="form-group">
-					<label class="col-md-4 control-label">Aluno da FACENS?</label>
-					<div class="col-md-4">
-						<div class="radio">
-							<label>
-								<input type="radio" name="hosting" value="yes" /> Sim
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="hosting" value="no" /> Não
-							</label>
-						</div>
-					</div>
-				</div>
-
-				<!-- Text area -->
-
-				<div class="alert alert-success" role="alert" id="success_message">Cadastro efetuado com sucesso <i class="glyphicon glyphicon-thumbs-up"></i> Obrigado por participar da Connect Facens.</div>
-
-				<!-- Button -->
-				<div class="form-group">
-					<label class="col-md-4 control-label"></label>
-					<div class="col-md-4">
-						<button type="submit" class="btn btn-warning" >Cadastrar <span class="glyphicon glyphicon-menu-right"></span></button>
-					</div>
-				</div>
-
-			</fieldset>
-		</form>
-	</div>
-</div><!-- /.container -->
-
-<div class="clearfix"></div>
-</div>
-
-
-</div>
-
+</script>
+<!--Script Ends-->
 
 </body>
-
-<script src="lib/bootstrap/js/bootstrap.js"></script>
-
-<style type="text/css">
-
-	body {
-		padding-top: 90px;
-	}
-	/* General sizing */
-	ul.dropdown-lr {
-		width: 300px;
-	}
-
-	/* mobile fix */
-	@media (max-width: 768px) {
-		.dropdown-lr h3 {
-			color: #eee;
-		}
-		.dropdown-lr label {
-			color: #eee;
-		}
-	}
-
-	.erro{
-		margin-top: 15px;
-		margin-left: 25px;
-	}
-
-</style>
 </html>
